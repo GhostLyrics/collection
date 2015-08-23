10ed0715303946ebdfe998214fc2604dd8452f8f
Alexander Skiba <mail@ghostlyrics.net>
2015-08-23 03:30:22 +0200

    Complete rewrite, skeleton is finished

--- extract-content-from-wordpress-mysql.py
+++ extract-content-from-wordpress-mysql.py
@@ -1,116 +1,190 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from xml.dom.minidom import parseString
-import MySQLdb as mdb
-import array
-import sys
+# Allow CAPS in function names
+# pylint: disable=C0103
+
+"""
+Export posts and pages from Wordpress MySQL database to text files.
+
+Auto-drafts are not exported.
+Allows to use a non standard prefix for Wordpress tables.
+
+Depends on MySQLdb module.
+"""
+
+import argparse
+import getpass
 import os
+import os.path
+import sys
+
+# non standard modules
+import MySQLdb as mdb
+
+def main():
+    """Export posts and pages from Wordpress MySQL database to text files."""
+
+    options = parse_arguments()
+    connection = None
+
+    try:
+        connection = connect(options)
+        print connection
+        authors = get_authors(connection, options.prefix)
+        posts = get_content(connection, "posts", options.prefix)
+        pages = get_content(connection, "pages", options.prefix)
+
+    except mdb.Error, error:
+        sys.exit("Error: {}".format(error))
+
+    finally:
+        if connection:
+            connection.close()
+
+    if posts:
+        export_content(posts, "posts", authors, options)
+    if pages:
+        export_content(pages, "pages", authors, options)
+
+    exit()
+
+def export_content(content, content_type, authors, options):
+    """Write the exported content to files."""
+
+    if os.path.exists("./{}".format(content_type)) is False:
+        os.mkdir(content_type)
+
+    for entry in content:
+
+        export = build_export(entry, authors, options, options.include_tags,
+            options.include_categories)
+
+        with open(os.path.join(content_type, entry["post_name"])
+                  + ".txt", "w") as textfile:
+            textfile.write(export)
 
 
-class connection:
-    def __init__(self):
-        with open('config.xml', 'r') as config:
-            xml = config.read()
-            dom = parseString(xml)
-            # read the server
-            xmlServer = dom.getElementsByTagName('server')[0].toxml()
-            self.server = xmlServer.replace('<server>',
-                                            '').replace('</server>', '')
-            # read the user
-            xmlUser = dom.getElementsByTagName('user')[0].toxml()
-            self.user = xmlUser.replace('<user>',
-                                        '').replace('</user>', '')
+def build_export(entry, authors, options, tags=None, categories=None):
+    """Construct the export text which is written to the file."""
 
-            # read the database name
-            xmlDatabase = dom.getElementsByTagName('database')[0].toxml()
-            self.database = xmlDatabase.replace('<database>',
-                                                '').replace('</database>', '')
+    export = "Title: {}\n".format(entry["post_title"])
+    export = export + "Date: {}\n".format(entry["post_date"])
+    export = export + "Author: {}\n".format(authors[entry["post_author"]])
 
-            # read the password. Use empty password if no tag exists,
-            # handle empty, non-valid XML strings by correcting to empty
-            # password.
-            try:
-                xmlPassword = dom.getElementsByTagName('password')[0].toxml()
-                password = xmlPassword.replace('<password>',
-                                               '').replace('</password>', '')
+    if tags:
+        tags_string = ""
+        for tag in tags:
+            tags_string = tags_string + tag + ","
+        export = export + "Tags: {}\n".format(tags_string)
 
-                if password == "<password/>":
-                    self.password = ""
-                    print "WARNING: Empty Strings are not valid XML."
-                    print "WARNING: Remove empty tag to use no password."
-                else:
-                    self.password = password
+    if categories:
+        category_string = ""
+        for category in categories:
+            category_string = category_string + category + ","
+        export = export + "Categories: {}\n".format(category_string)
 
-            except IndexError, e:
-                print "No password found: using empty password."
-                self.password = ""
+    if options.include_modified_date:
+        export = export + "Last Modified: {}\n".format(entry["post_modified"])
+    if options.include_published_url:
+        export = export + "Permalink: {}\n".format(entry["guid"])
 
+    export = export + "\n + {}\n".format(entry["post_content_filtered"])
 
-def exportDataset(row, path):
-    row["post_author"] = id_list.get(row["post_author"])
-    # using post_name here, this is the url version that should
-    # be safe to use for filenames
-    with open(path + "/" + row["post_name"] + ".txt", "w") as file_export:
+    return export
 
-        export_block = """
-Title: %s
+def get_authors(connection, prefix):
+    """Get a dictionary of authors from the database."""
 
-%s
+    authors = {}
 
-METADATA:
-Author: %s
-Source URL: %s
-Originally Published: %s
-Last Modified: %s""" % (row["post_title"], row["post_content"],
-                        row["post_author"], row["guid"], row["post_date"],
-                        row["post_modified"])
+    statement = "SELECT * FROM {}_users".format(prefix)
+    cursor = connection.cursor(mdb.cursors.DictCursor)
 
-        file_export.write(export_block)
+    cursor.execute(statement)
+    results = cursor.fetchall()
 
-con = None
-target = connection()
+    for user in results:
+        authors.update({int(user["ID"]): user["display_name"]})
+
+    return authors
 
 
-try:
-    con = mdb.connect(target.server, target.user,
-                      target.password, target.database)
+def get_content(connection, content_type, prefix):
+    """Get the posts/pages from the database."""
 
-    with con:
-        # DictCursor makes results of query accessible in dictionary instead
-        # of using tuples
-        cur = con.cursor(mdb.cursors.DictCursor)
-        cur.execute("SELECT * FROM journal_users")
-        authors = cur.fetchall()
-        id_list = {}
-        # fix system created posts having author ID '0'
-        for user in authors:
-            id_list.update({int(user["ID"]): user["display_name"]})
+    if content_type == "pages":
+        content_statement = ("SELECT * FROM {}_posts WHERE post_type ="
+            " 'page' AND post_status NOT LIKE 'auto-draft'".format(prefix))
 
-        # Handling posts of type "post", excluding auto-drafts here
-        cur.execute("SELECT * FROM journal_posts WHERE post_type = 'page' AND " +
-                    "post_status NOT LIKE 'auto-draft'")
+    elif content_type == "posts":
+        content_statement = ("SELECT * FROM {}_posts WHERE post_type ="
+            " 'post' AND post_status NOT LIKE 'auto-draft'".format(prefix))
 
-        pages = cur.fetchall()
-        for row in pages:
-            if os.path.exists("./pages") is False:
-                os.mkdir("pages")
-            exportDataset(row, "pages")
+    else:
+        raise TypeError("{} is not a recognized type of content.".format(
+            content_type))
 
-        # Handling posts of type "page", excluding auto-drafts again
-        cur.execute("SELECT * FROM journal_posts WHERE post_type = 'post' AND " +
-                    "post_status NOT LIKE 'auto-draft'")
+    cursor = connection.cursor(mdb.cursors.DictCursor)
+    cursor.execute(content_statement)
 
-        posts = cur.fetchall()
-        for row in posts:
-            if os.path.exists("./posts") is False:
-                os.mkdir("posts")
-            exportDataset(row, "posts")
+    content = cursor.fetchall()
+
+    return content
 
 
-except mdb.Error, e:
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
-finally:
-    if con:
-        con.close()
+def parse_arguments():
+    """Parse given command line arguments."""
+
+    text_server = "MySQL server you want to connect to"
+    text_user = ("Username used for authentication with the MySQL"
+                 "instance (default:root)")
+    text_database = "MySQL database you want to export data from"
+    text_prefix = "Prefix of your Wordpress table (default:wp)"
+    text_modified = "Include last modified date in the metadata block."
+    text_URL = "Include the published permalink in the metadata block."
+    text_tags = "Include tags for posts and pages."
+    text_categories = "Include categories for posts and pages."
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("server", help=text_server,)
+    parser.add_argument("database", help=text_database,)
+
+    parser.add_argument("-u", "--user", help=text_user, default="root")
+    parser.add_argument("-p", "--prefix", help=text_prefix, default="wp")
+
+    parser.add_argument("--include-published-url", help=text_URL,
+        action='store_true')
+    parser.add_argument("--include-modified-date", help=text_modified,
+        action='store_true')
+    parser.add_argument("-c", "--include-categories", help=text_categories,
+        action='store_true')
+    parser.add_argument("-t", "--include-tags", help=text_tags,
+        action='store_true')
+
+    arguments = parser.parse_args()
+
+    return arguments
+
+def ask_password(options):
+    """Get the password from stdin."""
+
+    password_text = "Password for {} on {}: ".format(options.user,
+        options.server)
+
+    password = getpass.getpass(password_text)
+
+    return password
+
+def connect(options):
+    """Open a connection to the database."""
+
+    password = ask_password(options)
+    connection = mdb.connect(options.server, options.user, password,
+        options.database)
+    print connection
+
+    return connection
+
+if __name__ == "__main__":
+    main()
